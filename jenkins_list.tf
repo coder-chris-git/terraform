bc56de800079d207af1464450578c1c2b9c8425c
coder-chris-git <cdav49719@gmail.com>
2023-12-24 11:32:25 -0500

    thanks

--- jenkins_list.tf
+++ jenkins_list.tf
@@ -18,53 +18,53 @@ resource "null_resource" "jenkins_job_1" {
   }
 }
 
-# resource "null_resource" "jenkins_job_2" {
-#   depends_on = [null_resource.jenkins_job_1]
+resource "null_resource" "jenkins_job_2" {
+  depends_on = [null_resource.jenkins_job_1]
 
 
 
 
 
-#   for_each = { for host, o in var.jenkins_list : "${o.name}_${o.jenkins_automation[1].job_name}" => o
-#     if o.number_of_jobs > 1
-#   }
+  for_each = { for host, o in var.jenkins_list : "${o.name}_${o.jenkins_automation[1].job_name}" => o
+    if o.number_of_jobs > 1
+  }
 
-#   provisioner "local-exec" {
-#     interpreter = ["/bin/sh", "-c"]
-#     command     = <<heredoc
-
-
-#     echo ${each.value.jenkins_automation[1].job_name}
-
-#     echo job finished
-
-#      heredoc
-
-#   }
-# }
+  provisioner "local-exec" {
+    interpreter = ["/bin/sh", "-c"]
+    command     = <<heredoc
 
 
+    echo ${each.value.jenkins_automation[1].job_name}
 
-# resource "null_resource" "jenkins_job_3" {
-#   depends_on = [null_resource.jenkins_job_2]
+    echo job finished
+
+     heredoc
+
+  }
+}
 
 
 
-
-#   for_each = { for host, o in var.jenkins_list : "${o.name}_${o.jenkins_automation[2].job_name}" => o
-#     if o.number_of_jobs > 2
-#   }
-
-#   provisioner "local-exec" {
-#     interpreter = ["/bin/sh", "-c"]
-#     command     = <<heredoc
+resource "null_resource" "jenkins_job_3" {
+  depends_on = [null_resource.jenkins_job_2]
 
 
-#     echo ${each.value.jenkins_automation[2].job_name}
 
-#     echo job finished
 
-#      heredoc
+  for_each = { for host, o in var.jenkins_list : "${o.name}_${o.jenkins_automation[2].job_name}" => o
+    if o.number_of_jobs > 2
+  }
 
-#   }
-# }
+  provisioner "local-exec" {
+    interpreter = ["/bin/sh", "-c"]
+    command     = <<heredoc
+
+
+    echo ${each.value.jenkins_automation[2].job_name}
+
+    echo job finished
+
+     heredoc
+
+  }
+}
