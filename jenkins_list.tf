3f4908cb73d08586943bd8115505fa2b4fe9c14b
coder-chris-git <cdav49719@gmail.com>
2023-12-24 11:16:31 -0500

    nsf

--- jenkins_list.tf
+++ jenkins_list.tf
@@ -1,25 +1,22 @@
-# resource "null_resource" "jenkins_job_1" {
+resource "null_resource" "jenkins_job_1" {
+
+  for_each = { for host, o in var.jenkins_list_1 : "${o.name}_${o.jenkins_automation[0].job_name}" => o
+    if o.jenkins_automation != null
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/sh", "-c"]
+    command     = <<heredoc
 
 
+    echo ${each.value.jenkins_automation[0].job_name}
 
+    echo job finished
 
-#   for_each = { for host, o in var.jenkins_list : "${o.name}_${o.jenkins_automation[0].job_name}" => o
-#     if o.jenkins_automation != null
-#   }
+     heredoc
 
-#   provisioner "local-exec" {
-#     interpreter = ["/bin/sh", "-c"]
-#     command     = <<heredoc
-
-
-#     echo ${each.value.jenkins_automation[0].job_name}
-
-#     echo job finished
-
-#      heredoc
-
-#   }
-# }
+  }
+}
 
 # resource "null_resource" "jenkins_job_2" {
 #   depends_on = [null_resource.jenkins_job_1]
