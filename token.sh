b1e799ad0afa7b996ea03419e540d6fa8e52b928
coder-chris-git <cdav49719@gmail.com>
2023-12-25 14:02:23 -0500

    test

--- token.sh
+++ token.sh
@@ -3,7 +3,6 @@
 # Load github secret from .env file
 set -a; source .env; set +a
 
-echo $GITHUB_TOKEN
 
 json='[{"name":"serv55","number_of_jobs":2,"token":"usertoken","user":"jenkins_user","jenkins_automation":[{"job_url":"job1_url","index":0,"job_name":"first_job_name","params":["first=first324_param1","second=first_param2"]},{"job_url":"secondurl","job_name":"s454_job_name","params":["first=first_param2","second=seco234nd_param2"],"token":"tok234en2","index":1,"user":"se432cond"}]}]'
 
